bad2c6df2bbb9a4c55440d76dc8d01f98c245819
Artem-Grushchinskiy <122301492+Artem-Grushchinskiy@users.noreply.github.com>
2023-04-11 16:24:34 +0200

    to check 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+const countriesEl = document.querySelector(`.country-list`);
+const countryEl = document.querySelector(`.country-info`);
+const check = document.querySelector(`#search-box`);
+check.addEventListener(`input`, debounce(mechanikal, 300));
+function mechanikal(event) {
+  if (event.target.value.trim() !== ``) {
+    fetchCountries(event.target.value.trim())
+      .then(responce => {
+        return responce.json();
+      })
+      .then(resp2 => {
+        workingCountries(resp2);
+      })
+      .catch(resp3 => {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      });
+  } else {
+  }
+}
+function workingCountries(resultCountries) {
+  if (resultCountries.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (resultCountries.length >= 2) {
+    renderCountries(resultCountries);
+  } else {
+    renderCountry(resultCountries[0]);
+  }
+}
+function renderCountries(arrCuont) {
+  let markUp = arrCuont
+    .map(el => {
+      return `<li class="view-countries">
+        <img class="img" src="${el.flags.svg}" alt="flag" />
+        <p class="title">${el.name.common}</p>
+      </li>`;
+    })
+    .join('');
+  countriesEl.innerHTML = markUp;
+  countryEl.innerHTML = ``;
+}
+function renderCountry(onlyOneCounty) {
+  let markUp = `<img class="img" src="${onlyOneCounty.flags.svg}" alt="" />
+      <p class="title">${onlyOneCounty.name.common}</p>
+      <ul>
+        <li>Capital: ${onlyOneCounty.capital}</li>
+        <li>Population: ${onlyOneCounty.population}</li>
+        <li>Languages: ${Object.values(onlyOneCounty.languages)}</li>
+      </ul>
+`;
+  countryEl.innerHTML = markUp;
+  countriesEl.innerHTML = ``;
+}
